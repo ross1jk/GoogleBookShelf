ee15101ce2b6c381d76f17612dc146ff91e36083
Jacqueline Ross <jacqueline.ross09@gmail.com>
2021-04-29 14:01:35 -0400

    links added to nav bar to each page styling to be added later

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="/">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-info">
+      <h1 className="navbar-brand">
         Google Book Shelf
-      </a>
+      </h1>
+      <Link to="/search" className={window.location.pathname === "/" ? "nav-link active" : "nav-link"} >
+        Search Books
+      </Link>
+      <Link
+        to="/saved"
+        className={window.location.pathname === "/about" ? "nav-link active" : "nav-link"}>
+        Saved Books
+      </Link>
     </nav>
   );
 }
