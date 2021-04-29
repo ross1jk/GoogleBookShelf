df92579426c6a0805ecbb67df64470544537b201
Jacqueline Ross <jacqueline.ross09@gmail.com>
2021-04-29 13:36:57 -0400

    routes are working in app.js

---
+++ client/src/pages/Saved.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Saved() {
+  return (
+    <p>saved test</p>
+  );
+}
+
+export default Saved;
---
+++ client/src/pages/Search.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Search() {
+  return (
+    <p>search test</p>
+  );
+}
+
+export default Search;
