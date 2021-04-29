943ca2eda455ffd50677f20cae99cb3cffa4011f
Jacqueline Ross <jacqueline.ross09@gmail.com>
2021-04-29 13:45:39 -0400

    bootstrap installed, jumbotron added

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Saved from "./pages/Saved";
 import Search from "./pages/Search";
 import Nav from "./components/Nav";
+import Jumbotron from "./components/Jumbotron"
 
 function App() {
   return (
     <Router>
       <div>
         <Nav />
+        <Jumbotron />
         <Switch>
           <Route exact path={["/", "/search"]}>
             <Search />
---
+++ client/src/components/Jumbotron/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Jumbotron() {
+  return (
+    <div className="jumbotron jumbotron-fluid">
+    <div className="container">
+      <h1 className="display-4">Google Book Shelf</h1>
+      <p className="lead">Search for and Save Books of the Internet</p>
+    </div>
+  </div>
+  );
+}
+
+export default Jumbotron;
