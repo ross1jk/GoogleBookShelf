7014a6f7e5b9905807dfb92ec5026689a395c52d
Jacqueline Ross <jacqueline.ross09@gmail.com>
2021-05-04 15:12:46 -0400

    updated with heroku login info

--- server.js
+++ server.js
@@ -17,7 +17,15 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebookshelf");
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/googlebookshelf", 
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // Start the API server
 app.listen(PORT, function() {
